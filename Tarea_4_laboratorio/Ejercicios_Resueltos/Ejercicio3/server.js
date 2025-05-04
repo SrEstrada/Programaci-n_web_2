114c226a900fd56e630499393b958ba2ad21f3bd
SrEstrada <sestradaa@unsa.edu.pe>
2025-05-04 00:09:57 -0500

    Completando el ejercicio3 de los Ejercicio Resueltos

---
+++ Tarea_4_laboratorio/Ejercicios_Resueltos/Ejercicio3/server.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bp = require('body-parser');
+const MarkdownIt = require('markdown-it'), md = new MarkdownIt();
+
+const app = express();
+
+app.use(express.static('pub'));
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log("Escuchando en: http://localhost:3000");
+});
+
+app.get('/', (request, response) => {
+  response.sendFile(path.resolve(__dirname, 'pub/index.html'));
+});
+
+app.post('/', (request, response) => {
+  const markDownText = request.body.text;
+  const htmlText = md.render(markDownText);
+  response.json({ text: htmlText });
+});
