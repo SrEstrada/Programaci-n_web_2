962340ea385cf15d2e6b29212d6b2fc063437f01
SrEstrada <sestradaa@unsa.edu.pe>
2025-05-06 01:15:21 -0500

    Agregando la funcionalidad del html y js para que muestre ya no por consola los datos, sino ya en la pantalla

--- Tarea_4_laboratorio/Ejercicios_DUTIC/Ejercicio2/script.js
+++ Tarea_4_laboratorio/Ejercicios_DUTIC/Ejercicio2/script.js
@@ -3,6 +3,8 @@ fetch('data.json')
   .then(data => {
     const region = data[0]; // Amazonas
     const total = region.confirmed.reduce((acc, dia) => acc + parseInt(dia.value), 0);
-    console.log("Total confirmados en", region.region, ":", total);
+    
+    const divResultado = document.getElementById('resultado');
+    divResultado.textContent = `Total de casos confirmados en ${region.region}: ${total}`;
   })
   .catch(error => console.error('Error cargando JSON:', error));
