10d8a873177b619cecc4d90fad8de7e262155332
SrEstrada <sestradaa@unsa.edu.pe>
2025-05-06 01:30:32 -0500

    Terminando el ejercicio 2 con la funcionalidad en su totalidad

--- Tarea_4_laboratorio/Ejercicios_DUTIC/Ejercicio2/script.js
+++ Tarea_4_laboratorio/Ejercicios_DUTIC/Ejercicio2/script.js
@@ -1,10 +1,16 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    const region = data[0]; // Amazonas
-    const total = region.confirmed.reduce((acc, dia) => acc + parseInt(dia.value), 0);
-    
-    const divResultado = document.getElementById('resultado');
-    divResultado.textContent = `Total de casos confirmados en ${region.region}: ${total}`;
+    const contenedor = document.getElementById('lista-confirmados');
+    const lista = document.createElement('ul');
+
+    data.forEach(region => {
+      const total = region.confirmed.reduce((sum, item) => sum + parseInt(item.value), 0);
+      const item = document.createElement('li');
+      item.textContent = `${region.region}: Casos confirmados = ${total}`;
+      lista.appendChild(item);
+    });
+
+    contenedor.appendChild(lista);
   })
-  .catch(error => console.error('Error cargando JSON:', error));
+  .catch(error => console.error('Error al cargar los datos:', error));
