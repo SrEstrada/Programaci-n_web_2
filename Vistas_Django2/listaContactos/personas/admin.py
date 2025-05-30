8c1ca9bda6538e328f51c71ed35f1c1851ed942f
SrEstrada <sestradaa@unsa.edu.pe>
2025-05-29 22:51:30 -0500

    feat(admin): Persona registrado en admin con configuración básica

--- Vistas_Django2/listaContactos/personas/admin.py
+++ Vistas_Django2/listaContactos/personas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Persona
 
-# Register your models here.
+@admin.register(Persona)
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "apellidos", "edad")
+    search_fields = ("nombre", "apellidos")
