b87109473b671f8ae44b0495a45fedea6a599073
SrEstrada <sestradaa@unsa.edu.pe>
2025-05-29 22:47:39 -0500

    feat(personas): Modelo Persona creado con campos básicos

--- Vistas_Django2/listaContactos/personas/models.py
+++ Vistas_Django2/listaContactos/personas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name="Nombre")
+    apellidos = models.CharField(max_length=100, verbose_name="Apellidos")
+    edad = models.PositiveIntegerField(verbose_name="Edad")
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellidos}"
+
+    class Meta:
+        verbose_name = "Persona"
+        verbose_name_plural = "Personas"
