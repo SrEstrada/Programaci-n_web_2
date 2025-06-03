1faaf0d8ee50912a33cea7efe36895309e8482a3
SrEstrada <sestradaa@unsa.edu.pe>
2025-06-02 21:51:56 -0500

    Agregación de mi carpeta assets para atrapar a todos mis archivos static y edición de mi html para las referencias correctas

--- Tarea_7_Laboratorio/mi_proyecto/mi_proyecto/settings.py
+++ Tarea_7_Laboratorio/mi_proyecto/mi_proyecto/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mi_proyecto_static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
