7a6b5ca6f029b4507c7c9b8ec629f0f43cd90a5b
SrEstrada <sestradaa@unsa.edu.pe>
2025-06-04 21:22:24 -0500

    Modelo Persona creado con campos: nombres, apellidos, edad y donador

--- Django_3/listaContactos/personas/models.py
+++ Django_3/listaContactos/personas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    edad = models.IntegerField(blank=True)
+    donador = models.BooleanField(default=False)  # Añadí default=False
