377658c4cd52f8ada2d32bfc0313fcfcf6e30a46
SrEstrada <sestradaa@unsa.edu.pe>
2025-06-05 00:35:22 -0500

    Medelo Persona creado, migraciones aplicadas y panel de admin configurado

--- Django_3/listaContactos/personas/models.py
+++ Django_3/listaContactos/personas/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Persona(models.Model):
     nombres = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
-    edad = models.IntegerField(blank=True)
-    donador = models.BooleanField(default=False)  
+    edad = models.IntegerField(blank=True, null=True) 
+    donador = models.BooleanField(default=False)      
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"     
