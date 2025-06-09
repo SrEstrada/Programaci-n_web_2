04c1f40de86ae21bef481f63addbbc054fab37cd
SrEstrada <sestradaa@unsa.edu.pe>
2025-06-08 22:13:35 -0500

    Hice mis modelos de UnoAMuchos y además hice las migraciones correspondientes (registre la app UnoAMuchos)

--- Tarea_8_Laboratorio/Lab8/UnoAMuchos/models.py
+++ Tarea_8_Laboratorio/Lab8/UnoAMuchos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Language(models.Model):
+    name = models.CharField(max_length=50)
 
+class Framework(models.Model): 
+    name = models.CharField(max_length=50)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
