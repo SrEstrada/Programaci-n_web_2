c0a5ea0477c8988ae0a6ee1d7e30a75c4b5741b1
SrEstrada <sestradaa@unsa.edu.pe>
2025-06-08 23:41:18 -0500

    Creación de mis vistas y templates para generar los pdf, además de mi configuración en las urls

--- Tarea_8_Laboratorio/CreadorPDF/pdf_app/views.py
+++ Tarea_8_Laboratorio/CreadorPDF/pdf_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from pdf_project.renderers import render_to_pdf
+import datetime
 
-# Create your views here.
+def generar_pdf(request):
+    data = {
+        'today': datetime.date.today(),
+        'amount': 99.99,
+        'customer_name': 'Juan Pérez',
+        'invoice_number': 123456,
+        'pdf_title': 'Factura de Venta'
+    }
+    return render_to_pdf('pdfs/invoice.html', data)
--- Tarea_8_Laboratorio/CreadorPDF/pdf_project/urls.py
+++ Tarea_8_Laboratorio/CreadorPDF/pdf_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from pdf_app.views import generar_pdf
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pdf/', generar_pdf, name='generar_pdf'),
 ]
