8ff8f5922e203fe8d8bcb9271511f07c23791679
SrEstrada <sestradaa@unsa.edu.pe>
2025-06-22 22:21:46 -0500

    Agregación de mi css a mi página web, redireción automático cuando guardo a una persona y actualización de mi personasList.html

--- git_de_Django_4_y_5/Django4/listaContactos/personas/views.py
+++ git_de_Django_4_y_5/Django4/listaContactos/personas/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from .forms import RawPersonaForm
 from .models import Persona
+from django.shortcuts import redirect  
 
 def personasAnotherCreateView(request):
     if request.method == "POST":
         form = RawPersonaForm(request.POST)
         if form.is_valid():
-            Persona.objects.create(**form.cleaned_data)  # Guarda en DB
+            Persona.objects.create(**form.cleaned_data)
+            return redirect('lista_personas')  # Redirige a la lista
     else:
-        form = RawPersonaForm()  # Formulario vacío para GET
+        form = RawPersonaForm()
     return render(request, 'personas/personasCreate.html', {'form': form})
 def personasListView(request):
     queryset = Persona.objects.all()
